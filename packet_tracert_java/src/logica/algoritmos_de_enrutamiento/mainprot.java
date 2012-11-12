8b461333a48581a106bd2dcc2fbd83e08b54d7b4
minrock <minrock@minrock-HP-420>
2012-11-11 21:24:45 -0500

    Agregado el principio de Rip y RipV2... falta implementacion en los routers.

---
+++ packet_tracert_java/src/logica/algoritmos_de_enrutamiento/mainprot.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica.algoritmos_de_enrutamiento;
+
+/**
+ *
+ * @author minrock
+ */
+public class mainprot {
+    
+    public static void main(String s[]){
+        RipTable rip = new RipTable("192.168.1.1");
+        rip.enableRipV2();
+        rip.add_rip_Entrance(3, "192.168.0.0","24", "192.168.1.1");
+        rip.imprimir();
+        rip.compare_entrances(new RipEntrance(1, "192.168.0.0", "22", "192.168.3.1", "192.168.2.1"));
+        rip.imprimir();
+    }
+    
+}
